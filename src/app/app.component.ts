0dd35ef803353554b08af9c55034cb28b667289a
冯兆海 <fengzhaohai@fengzhaohaideMacBook-Air.local>
2017-02-16 15:13:34 +0800

    添加ngModel模块，双向绑定example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
 
+export class Person {
+  name: string;
+  constructor(
+    public id: Number,
+    public age: Number
+  ) {
+    this.name = 'Tony';
+  }
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<ul>
+                  <li *ngFor="let person of persons">
+                    {{ person.name }} {{ person.age }} {{person.id}}
+                    <input [(ngModel)]="person.name">
+                  </li>
+                </ul>
+             `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  persons = [
+    new Person(2, 22),
+    new Person(3, 23),
+    new Person(3, 2322)
+  ];
+}
